088104f7a9103a41153d51081245774e4b7080b2
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2024-02-05 21:59:30 +0000

    chore: #1: Set up E2E solution with some initial tests.

---
+++ Atlas.Auto.Tests/TestsSetUp.cs
@@ -0,0 +1,14 @@
+﻿using Atlas.Auto.Tests.DependencyInjection;
+
+namespace Atlas.Auto.Tests
+{
+    [SetUpFixture]
+    public class TestsSetUp
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUpForAllTests()
+        {
+            DependencyInjection.DependencyInjection.Provider = ServiceConfiguration.CreateProvider();
+        }
+    }
+}
