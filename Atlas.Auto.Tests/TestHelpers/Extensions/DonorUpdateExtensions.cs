5e8461936835ba8ecc02c6d207abcebf8099403d
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2024-02-14 20:24:49 +0000

    chore: #9: Assert present donor info is as expected.

--- Atlas.Auto.Tests/TestHelpers/Extensions/DonorUpdateExtensions.cs
+++ Atlas.Auto.Tests/TestHelpers/Extensions/DonorUpdateExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Atlas.DonorImport.FileSchema.Models;
+﻿using Atlas.Debug.Client.Models.DonorImport;
+using Atlas.DonorImport.FileSchema.Models;
 
 namespace Atlas.Auto.Tests.TestHelpers.Extensions
 {
@@ -7,6 +8,23 @@ namespace Atlas.Auto.Tests.TestHelpers.Extensions
         public static IReadOnlyCollection<string> GetExternalDonorCodes(this IEnumerable<DonorUpdate> updates)
         {
             return updates.Select(u => u.RecordId).ToList();
+        }
+
+        public static IEnumerable<DonorDebugInfo> ToDonorDebugInfo(this IEnumerable<DonorUpdate> updates)
+        {
+            return updates.Select(ToDonorDebugInfo);
+        }
+
+        public static DonorDebugInfo ToDonorDebugInfo(this DonorUpdate update)
+        {
+            return new DonorDebugInfo
+            {
+                ExternalDonorCode = update.RecordId,
+                DonorType = update.DonorType.ToString(),
+                RegistryCode = update.RegistryCode,
+                EthnicityCode = update.Ethnicity,
+                Hla = update.Hla.ToPhenotypeInfoTransfer()
+            };
         }
     }
 }
