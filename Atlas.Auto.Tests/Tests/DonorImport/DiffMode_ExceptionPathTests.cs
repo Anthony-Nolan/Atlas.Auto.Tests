c716a51a1d83689f5ff386207322382cd3d4d0d7
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2024-02-19 14:34:47 +0000

    tests: #2: Create existing donor in diff mode test.

---
+++ Atlas.Auto.Tests/Tests/DonorImport/DiffMode_ExceptionPathTests.cs
@@ -0,0 +1,58 @@
+﻿using Atlas.Auto.Tests.DependencyInjection;
+using Atlas.Auto.Tests.TestHelpers.Builders;
+using Atlas.Auto.Tests.TestHelpers.Extensions;
+using Atlas.Auto.Tests.TestHelpers.SourceData;
+using Atlas.Auto.Tests.TestHelpers.Workflows;
+using Atlas.DonorImport.FileSchema.Models;
+
+namespace Atlas.Auto.Tests.Tests.DonorImport;
+
+/// <summary>
+/// Tests that cover exception paths of Atlas donor import when in diff mode.
+/// </summary>
+[TestFixture]
+[Category($"{TestConstants.DonorImportTestTag}_{nameof(DiffMode_ExceptionPathTests)}")]
+// ReSharper disable once InconsistentNaming
+internal class DiffMode_ExceptionPathTests
+{
+    private IServiceProvider serviceProvider;
+    private IDonorImportWorkflow donorImportWorkflow;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        serviceProvider = ServiceConfiguration.CreateProvider();
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        donorImportWorkflow = serviceProvider.ResolveServiceOrThrow<IDonorImportWorkflow>();
+    }
+
+    [Test]
+    public async Task DonorImport_DiffMode_CreateExistingDonor_FailedTheUpdate()
+    {
+        const int donorCount = 1;
+        const ImportDonorChangeType changeType = ImportDonorChangeType.Create;
+
+        // import the same donor create update twice
+
+        // first time: both import and creation update should succeed
+        var creationUpdate = DonorUpdateBuilder.Default
+            .WithValidDnaAtAllLoci()
+            .WithChangeType(changeType)
+            .Build(donorCount);
+
+        var firstImportRequest = await donorImportWorkflow.ImportDiffDonorFile(creationUpdate);
+        await donorImportWorkflow.DonorImportWasSuccessful(firstImportRequest.FileName, donorCount, 0);
+
+        var expectedDonorInfo = creationUpdate.ToDonorDebugInfo().ToList();
+        await donorImportWorkflow.DonorStoreShouldHaveExpectedDonors(expectedDonorInfo);
+        await donorImportWorkflow.DonorsShouldBeAvailableForSearch(expectedDonorInfo);
+
+        // second time: the import should succeed, but the update should fail validation
+        var secondImportRequest = await donorImportWorkflow.ImportDiffDonorFile(creationUpdate);
+        await donorImportWorkflow.DonorImportWasSuccessful(secondImportRequest.FileName, 0, donorCount);
+    }
+}
