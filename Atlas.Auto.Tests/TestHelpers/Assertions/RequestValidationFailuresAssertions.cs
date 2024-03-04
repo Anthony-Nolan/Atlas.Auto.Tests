914d08c604a14c577c41a309f08a13ff43bd24fa
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2024-03-04 13:23:47 +0000

    tests: #22: Added exception test for invalid search request.

---
+++ Atlas.Auto.Tests/TestHelpers/Assertions/RequestValidationFailuresAssertions.cs
@@ -0,0 +1,14 @@
+﻿using Atlas.Debug.Client.Models.Validation;
+using FluentAssertions;
+
+namespace Atlas.Auto.Tests.TestHelpers.Assertions
+{
+    internal static class RequestValidationFailuresAssertions
+    {
+        public static void ShouldContain(this IReadOnlyCollection<RequestValidationFailure> validationFailures, string validationFailure)
+        {
+            validationFailures.Should().NotBeNullOrEmpty();
+            validationFailures.Should().Contain(failure => failure.ErrorMessage == validationFailure);
+        }
+    }
+}
