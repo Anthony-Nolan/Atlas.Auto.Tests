02adafc6f798becaa56956a2cdc399371e7cc749
pchenery <peter.chenery@anthonynolan.org>
2024-12-24 16:59:59 +0000

    feature: CBT-3442: Change scoring tests to call the Atlas public API endpoints

--- Atlas.Auto.Tests/TestHelpers/Services/Scoring/DonorScorer.cs
+++ Atlas.Auto.Tests/TestHelpers/Services/Scoring/DonorScorer.cs
@@ -1,35 +1,49 @@
 ﻿using Atlas.Auto.Tests.TestHelpers.InternalModels;
+using Atlas.Client.Models.Scoring.Requests;
+using Atlas.Client.Models.Scoring.Results;
 using Atlas.Debug.Client.Clients;
-using Atlas.MatchingAlgorithm.Client.Models.Scoring;
 
 namespace Atlas.Auto.Tests.TestHelpers.Services.Scoring;
 
 internal interface IDonorScorer
 {
-    Task<DebugResponse<IEnumerable<DonorScoringResult>>> ScoreBatch(BatchScoringRequest request);
+    Task<DebugResponse<IEnumerable<DonorScoringResult>>> ScoreBatch(DonorHlaBatchScoringRequest request);
+
+    Task<DebugResponse<ScoringResult>> Score(DonorHlaScoringRequest request);
 }
 
 internal class DonorScorer : IDonorScorer
 {
     private readonly IDebugRequester debugRequester;
-    private readonly IMatchingAlgorithmFunctionsClient matchingClient;
+    private readonly IPublicApiFunctionsClient publicApiClient;
 
     public DonorScorer(
-        IDebugRequester debugRequester, 
-        IMatchingAlgorithmFunctionsClient matchingClient)
+        IDebugRequester debugRequester,
+        IPublicApiFunctionsClient publicApiClient)
     {
         this.debugRequester = debugRequester;
-        this.matchingClient = matchingClient;
+        this.publicApiClient = publicApiClient;
     }
 
-    public async Task<DebugResponse<IEnumerable<DonorScoringResult>>> ScoreBatch(BatchScoringRequest request)
+    public async Task<DebugResponse<IEnumerable<DonorScoringResult>>> ScoreBatch(DonorHlaBatchScoringRequest request)
     {
-        return await debugRequester.ExecuteDebugRequestWithWaitAndRetry(10, 20, async () => await Score(request));
+        return await debugRequester.ExecuteDebugRequestWithWaitAndRetry(10, 20, async () => await PostScoreBatch(request));
     }
 
-    private async Task<DebugResponse<IEnumerable<DonorScoringResult>>> Score(BatchScoringRequest request)
+    public async Task<DebugResponse<ScoringResult>> Score(DonorHlaScoringRequest request)
     {
-        var result = await matchingClient.ScoreBatch(request);
+        return await debugRequester.ExecuteDebugRequestWithWaitAndRetry(10, 20, async () => await PostScore(request));
+    }
+
+    private async Task<DebugResponse<IEnumerable<DonorScoringResult>>> PostScoreBatch(DonorHlaBatchScoringRequest request)
+    {
+        var result = await publicApiClient.PostScoreBatch(request);
         return new DebugResponse<IEnumerable<DonorScoringResult>>(result);
+    }
+
+    private async Task<DebugResponse<ScoringResult>> PostScore(DonorHlaScoringRequest request)
+    {
+        var result = await publicApiClient.PostScore(request);
+        return new DebugResponse<ScoringResult>(result);
     }
 }
