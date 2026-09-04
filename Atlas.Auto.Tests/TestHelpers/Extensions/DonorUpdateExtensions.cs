4b865a8089ea3c1175b065bc832ca38d9dfd89c4
Roman Sivak <Roman.Sivak@anthonynolan.org>
2026-09-04 13:21:03 +0200

    fix ATL-336

--- Atlas.Auto.Tests/TestHelpers/Extensions/DonorUpdateExtensions.cs
+++ Atlas.Auto.Tests/TestHelpers/Extensions/DonorUpdateExtensions.cs
@@ -1,4 +1,4 @@
-﻿using Atlas.Debug.Client.Models.DonorImport;
+using Atlas.Debug.Client.Models.DonorImport;
 using Atlas.DonorImport.FileSchema.Models;
 
 namespace Atlas.Auto.Tests.TestHelpers.Extensions
@@ -17,19 +17,20 @@ namespace Atlas.Auto.Tests.TestHelpers.Extensions
 
         public static DonorDebugInfo ToDonorDebugInfo(this DonorUpdate update)
         {
+            var (externalDonorCode, donorType, registryCode, ethnicityCode) = GetCommonFields(update);
             return new DonorDebugInfo
             {
-                ExternalDonorCode = update.RecordId,
-                DonorType = update.DonorType.ToString(),
-                RegistryCode = update.RegistryCode,
-                EthnicityCode = update.Ethnicity,
+                ExternalDonorCode = externalDonorCode,
+                DonorType = donorType,
+                RegistryCode = registryCode,
+                EthnicityCode = ethnicityCode,
                 Hla = update.Hla.ToPhenotypeInfoTransfer()
             };
         }
 
         public static IEnumerable<FailedDonorUpdate> ToFailureInfo(
             this IEnumerable<DonorUpdate> updates,
-            string failedPropertyName, 
+            string failedPropertyName,
             string failureReason)
         {
             return updates.Select(u => u.ToFailureInfo(failedPropertyName, failureReason));
@@ -37,15 +38,20 @@ namespace Atlas.Auto.Tests.TestHelpers.Extensions
 
         public static FailedDonorUpdate ToFailureInfo(this DonorUpdate update, string failedPropertyName, string failureReason)
         {
+            var (externalDonorCode, donorType, registryCode, ethnicityCode) = GetCommonFields(update);
             return new FailedDonorUpdate
             {
-                ExternalDonorCode = update.RecordId,
-                DonorType = update.DonorType.ToString(),
-                RegistryCode = update.RegistryCode,
-                EthnicityCode = update.Ethnicity,
+                ExternalDonorCode = externalDonorCode,
+                DonorType = donorType,
+                RegistryCode = registryCode,
+                EthnicityCode = ethnicityCode,
                 PropertyName = failedPropertyName,
                 FailureReason = failureReason
             };
         }
+
+        private static (string ExternalDonorCode, string DonorType, string RegistryCode, string EthnicityCode) GetCommonFields(
+            DonorUpdate update) =>
+            (update.RecordId, update.DonorType.ToString(), update.RegistryCode, update.Ethnicity);
     }
 }
